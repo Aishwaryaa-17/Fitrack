9250e9cf73ed12122a9bd33f23f65a425fe33b60
Aishwaryaa-17 <143827149+Aishwaryaa-17@users.noreply.github.com>
2025-06-25 23:03:21 +0530

    fix: url updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -329,7 +329,6 @@ const Navbar = ({ currentUser }) => {
         <MobileMenu isOpen={isOpen}>
           <Navlink to="/">Dashboard</Navlink>
           <Navlink to="/workouts">Workouts</Navlink>
-          <Navlink to="/tutorials">Tutorials</Navlink>
           <Navlink to="/blogs">Blogs</Navlink>
           <Navlink to="/contact">Contact</Navlink>
         </MobileMenu>
